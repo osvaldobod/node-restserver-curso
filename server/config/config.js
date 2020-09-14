29217d4881a35ca84583c7b721075ca6988bfca0
Osvaldo <obodratti@santandertecnologia.com.ar>
2020-09-14 11:23:22 -0300

    correccion variable entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = PROCESS.env.MONGO_URI;
+    urlDB = process.env.MONGO_URI;
 
 };
 
