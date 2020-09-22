527984c9a1bcf6e2d28c89b6403b0ffa7e6f6f12
Osvaldo <obodratti@santandertecnologia.com.ar>
2020-09-22 17:54:04 -0300

    logon fix

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -7,11 +7,6 @@ const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticac
 const app = express();
 app.get('/usuario', verificaToken, (req, res) => {
 
-    return res.json({
-        usuario: req.usuario,
-        nombre: req.usuario.nombre,
-        email: req.usuario.email
-    });
 
     //    res.json('get Usuario LOCAL');
     let desde = req.query.desde || 0;
